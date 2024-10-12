18febe5bedd74c9bff66507872b7f3680ce1a297
admirdemir <admirdemir@gmail.com>
2024-10-12 19:15:51 +0200

    Add test files for Jest and Supertest

---
+++ tests/unit/example.test.ts
@@ -0,0 +1,9 @@
+// tests/unit/example.test.ts
+
+import { describe, it, expect } from "@jest/globals"; // Import from Jest
+
+describe("Example Unit Test", () => {
+  it("should return true", () => {
+    expect(true).toBe(true);
+  });
+});
